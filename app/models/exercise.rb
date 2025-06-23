19a4da341438b5b21864106345333a4c5a7b6b5d
Santiago Morales <smorales@firstrate.com>
2025-06-23 14:29:42 -0400

    minor refactor

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -7,6 +7,10 @@ class Exercise < ApplicationRecord
   has_many :exercise_sets, dependent: :destroy
   has_one_attached :image
 
+  def max_load
+    self.exercise_sets.maximum(:load)
+  end
+
   # attributes: 
   # name String
   # notes String
