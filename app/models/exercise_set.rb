d9373a7dc1d96c4ed240fa1adc69bbaffcc76e25
TagoHub <tago470@gmail.com>
2026-01-08 22:06:42 -0300

    improved reps n loads suggestions

--- app/models/exercise_set.rb
+++ app/models/exercise_set.rb
@@ -65,14 +65,17 @@ class ExerciseSet < ApplicationRecord
   end
 
   def suggested_load
+    return load if (intensity && suggested_intensity) && (intensity > suggested_intensity) || updated_at.to_date == Date.today
     user = exercise.exercise_group.workout.program.user
-    return load if (intensity && suggested_intensity) && (intensity > suggested_intensity)
-    additional_max_load = exercise.max_load || 0.0
+    loads = exercise.working_sets.pluck(:load)
+    avg_load = (loads.sum / loads.size) || 0.0
     instrument_weight = get_load(exercise.instrument.weight, exercise.instrument.unit, unit) || 0
     body_weight = exercise.body_weight ? get_load(user.weight || 70, user.weight_unit || Unit.find_by(name: "Kilograms"), unit) : 0
-    max_load = additional_max_load + instrument_weight + body_weight
-    if working_set?
-      (exercise.maxed_rep_range? && !last_set?) ? round_to_half(additional_max_load + 0.1*max_load) : additional_max_load
+    max_load = avg_load + instrument_weight + body_weight
+    if first_working_set?
+      suggested_reps == exercise.rep_range_min ? round_to_half(avg_load + 0.1*max_load) : avg_load
+    elsif working_set?
+      first_working_set.load
     else
       suggested_warmup = ExerciseSet::WARMUP[warmup_type_count - 1]
       suggested_load = max_load * suggested_warmup[order - 1][:relative_load] - (instrument_weight + body_weight)
@@ -94,21 +97,25 @@ class ExerciseSet < ApplicationRecord
   end
 
   def suggested_reps
-    return reps if intensity && suggested_intensity && (intensity > suggested_intensity)
-    if working_set?
-      if exercise.maxed_rep_range? && !last_set?
-        exercise.rep_range_min
-      elsif exercise.same_reps? && !last_set?
-        exercise.max_reps + 1
-      elsif exercise.max_load && (load < exercise.max_load)
-        exercise.working_sets.where(load: exercise.max_load).first.reps
-      else
-        exercise.max_reps
-      end
+    return reps if intensity && suggested_intensity && (intensity > suggested_intensity) || updated_at.to_date == Date.today
+    if first_working_set?
+      all_reps = exercise.working_sets.pluck(:reps)
+      avg_reps = (all_reps.sum / all_reps.size).to_i + 1
+      (avg_reps > exercise.rep_range_max) ? exercise.rep_range_min : avg_reps
+    elsif working_set?
+      first_working_set.reps
     else
       suggested_warmup = ExerciseSet::WARMUP[warmup_type_count - 1]
       suggested_warmup[order - 1][:reps].to_i
     end
+  end
+
+  def first_working_set?
+    first_working_set == self
+  end
+
+  def first_working_set
+    exercise.exercise_sets.where(set_type: "Working Set").order(:order).first
   end
 
   def warmup_type_count
